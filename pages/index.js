967a4b392d211fdb28c94aac856c2d542450d60f
ryanmbigelow <ryanmbigelow@gmail.com>
2023-01-13 11:16:57 -0600

    completed for week 13

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,30 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
+import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
+import { useState } from 'react';
 // import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  const [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue((prevState) => prevState + 1);
+  };
+
+  // const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
+    <div>
+      <>
+        <h1>Hello {user.displayName}! </h1>
+      </>
+      <>
+        <h1>Counter Title</h1>
+        <h2>{value}</h2>
+        <Button type="button" onClick={handleClick}>Increment</Button>
+        <Button type="button" onClick={() => setValue((prevState) => prevState - 1)}>Decrement</Button>
+        <Button type="button" onClick={() => setValue(0)}>Reset</Button>
+      </>
     </div>
   );
 }
